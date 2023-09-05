a6ca06384f7dce71d777b3b6ffc035548c57a1f1
quangvuong2541 <truongquangvuong2000@gmail.com>
2023-09-05 22:06:56 +0700

    reduxsage

--- src/index.js
+++ src/index.js
@@ -5,9 +5,15 @@ import App from './App';
 import reducer from './store/reducer';
 
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import createSageMidleware from 'redux-saga'
+import { watchAgeUp } from './sagas/sagas';
 
-const store = createStore(reducer);
+const sagaMiddleWare = createSageMidleware()
+const store = createStore(reducer, applyMiddleware(sagaMiddleWare));
+sagaMiddleWare.run(watchAgeUp)
+
+
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
